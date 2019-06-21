b1fe6ecd32a0ed0d690c2bbd4fb9c785eb3e815e
Sam Brown <stbrown463@gmail.com>
2019-06-21 17:16:51 -0500

    started firebase setup

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Note from "./Note/Note";
 import NoteForm from "./NoteForm/NoteForm";
+import { DB_CONFIG } from './Config/config';
+import firebase from 'firebase/app';
+import 'firebase/database'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.addNote = this.addNote.bind(this);
+    this.app = firebase.initializeApp(DB_CONFIG);
+    this.database = this.app.database().ref().child('notes');
 
     this.state = {
-      notes: [
-        { id: 1, noteContent: "Note 1 here!" },
-        { id: 2, noteContent: "Note 2 here!" },
-      ]
+      notes: []
     }
+  }
+
+  componentWillMount(){
+    const previousNotes = this.state.notes;
+
+    // DataSnapshot
+    this.database.on('child_added', snap => {
+      previousNotes.push({
+        id: snap.key,
+        noteContent: snap.val().noteContent,
+      })
+
+      this.setState({
+        notes: previousNotes
+      })
+    })
   }
 
   addNote(note){
     // push the note onto the notes array.
-    const previousNotes = this.state.notes;
-    previousNotes.push({ id: previousNotes.length + 1, noteContent: note });
-    this.setState({
-      notes: previousNotes
-    })
+    // const previousNotes = this.state.notes;
+    // previousNotes.push({ id: previousNotes.length + 1, noteContent: note });
+    // this.setState({
+    //   notes: previousNotes
+    // })
+
+    this.database.push().set({ noteContent: note })
+
   }
 
   render() {
