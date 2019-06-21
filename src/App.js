e7500d4093d76731a437c3a88d77fdea7ab7ea6a
Sam Brown <stbrown463@gmail.com>
2019-06-21 16:16:23 -0500

    notes displaying

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Note from "./Note/Note";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to Notes</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        { id: 1, noteContent: "Note 1 here!" },
+        { id: 2, noteContent: "Note 2 here!" },
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="notesWrapper">
+        <div className="notesHeader">
+          <div className="heading">React and Firebase To Do List</div>
+        </div>
+        <div className="notesBody">
+          {
+            this.state.notes.map((note) => {
+              return (
+                <Note noteContent={note.noteContent} noteId={note.id} key={note.id} />
+              )
+            })
+          }
+        </div>
+        <div className="notesFooter">
+          Footer will go here...
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/NoteForms/NoteForms.js
@@ -0,0 +1,1 @@
+NoteForms.js
